76da067797c5df2762b98d42e945c4ce5bd8212d
CTS458701 <neerajkumar.nittoori@cognizant.com>
2015-10-12 15:28:11 +0530

    GarbageCollection

---
+++ Day3Mrng/GarbageCollection/GarbageCollection/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GarbageCollection
+{
+	class Garbage1
+	{
+		int[] a1 = new int[100];
+		string[] a2 = new string[100];
+	}
+	class Garbage2:Garbage1
+	{
+		int[] b = new int[10000];
+	}
+
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			MainClass.createGarbage (1000);
+			Console.WriteLine (GC.GetTotalMemory(false));
+			GC.Collect();
+			Console.WriteLine (GC.GetTotalMemory(false));
+
+		}
+
+		static void createGarbage(int x)
+		{
+			Garbage1 garb;
+			for (int i = 0; i < x; i++) 
+			{
+				garb = new Garbage1 ();
+			}
+		}
+
+	}
+
+}
