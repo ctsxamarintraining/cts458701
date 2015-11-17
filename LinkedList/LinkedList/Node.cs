be3d69212ede2ad244ec89a0184da2bcaa27a068
CTS458701 <neerajkumar.nittoori@cognizant.com>
2015-11-17 12:39:21 +0530

    LinkedList
    LinkedListwithourenumarator

---
+++ LinkedList/LinkedList/Node.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace LinkedList
+{
+	public class Node
+	{
+		public int data;
+		Node next;
+		public Node (int value)
+		{
+			next = null;
+			data = value;
+
+		}
+
+		public void addAnObject(int ValuetobeAdded)
+		{
+
+
+			if (next == null) {
+
+				next = new Node (ValuetobeAdded);
+
+
+			} else {
+
+
+
+				next.addAnObject (ValuetobeAdded);
+
+
+
+			}
+
+		}
+
+
+		public void PrintTheValues()
+		{
+			Console.WriteLine (data);
+			if (next != null) {
+				next.PrintTheValues ();
+
+			}
+
+
+
+		}
+
+	}
+}
+
---
+++ LinkedList/LinkedList/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace LinkedList
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			Node myNode = new Node(1);
+			myNode.addAnObject (2);
+			myNode.addAnObject (3);
+			myNode.addAnObject (4);
+			myNode.PrintTheValues ();  
+		}
+	}
+}
